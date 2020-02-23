462b875b7c903493533d45597dfab31731f4aa58
Sergio Romero <sromerogarcia@ucmerced.edu>
2020-02-22 23:10:08 -0800

    First version of wear depth plot

--- wear_depth_plot.py
+++ wear_depth_plot.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-# Data for plotting
-t = np.arange(50, 300, 50)
-s = np.array([0.791, 0.840, 0.857, 0.8635, 0.8545])
-e = [0.1407, 0.1061, 0.0827, 0.1223, 0.1506]
+# Data Undoped for plotting
+t1 = np.arange(50, 300, 50)
+s1 = np.array([0.791, 0.840, 0.857, 0.8635, 0.8545])
+e1 = [0.1407, 0.1061, 0.0827, 0.1223, 0.1506]
 
+# Data Ni-doped for plotting
+t2 = np.arange(50, 300, 50)
+s2 = np.array([0.4435, 0.707, 0.594, 0.5705, 0.5465])
+e2 = [0.1944, 0, 0.2758, 0.3118, 0.3189]
 
 fig, ax = plt.subplots()
 
 ax.set_ylim(1.2,0)
-ax.plot(t, s, 'o')
-ax.errorbar(t, s, yerr=e,fmt='-')
+ax.plot(t1, s1, 'o')
+ax.errorbar(t1, s1, yerr=e1,fmt='-')
 # arr_img = mpimg.imread('UD_HS_AM_RT_17mm_50rev.png')
 
 ax.set(xlabel='Revolutions', ylabel="Wear depth ($\mu$m)",
